f2994adee96724a8a069b7ffb82d86d178b24a43
eleazar harold <haroldyewa@gmail.com>
2020-08-05 03:20:04 +0300

    solution to GCD updated comments.

--- gcd.go
+++ gcd.go
@@ -8,7 +8,7 @@ func GCD(a, b int) int {
 	// step 2: a becomes b and b becomes the remainder of dividing a by b
 	a, b = b, a % b
 	// step 3: go to step 1
-	return GCD(a, b)
+	return GCD(a, b) // recursion
 
 	// solution 2 using euclidean algorithm
 	// for b != 0 {
