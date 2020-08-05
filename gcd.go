d06943995d66b2781fa2b1503bbadf2591132bbc
eleazar harold <haroldyewa@gmail.com>
2020-08-05 03:19:14 +0300

    solution to GCD.

---
+++ gcd.go
@@ -0,0 +1,18 @@
+package module01
+
+func GCD(a, b int) int {
+	// step 1: if b == 0, return a
+	if b == 0 {
+		return a
+	}
+	// step 2: a becomes b and b becomes the remainder of dividing a by b
+	a, b = b, a % b
+	// step 3: go to step 1
+	return GCD(a, b)
+
+	// solution 2 using euclidean algorithm
+	// for b != 0 {
+	// 	a, b = b, a%b
+	// }
+	// return a
+}
